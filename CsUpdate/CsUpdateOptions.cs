c817ce07b3995e98145fe7affc2a545b553fcc8b
Peter Tiedemann <pt@configit.com>
2015-12-23 20:45:07 +0100

    Fix --help formatting for csupdate.

--- CsUpdate/CsUpdateOptions.cs
+++ CsUpdate/CsUpdateOptions.cs
@@ -18,8 +18,8 @@ namespace CsUpdate {
     [OptionList( 'p', "packages", ',',
       HelpText = "Optionally specifies a list of package specifications limiting the packages that will be upgraded or aligned. "
                  + "Each item in the list can either be a package id, or a tuple of the form <id>:<version> or <id>:<version>:<framework>. "
-                 + "For example, the input could be\nA,B.C:1.2.0:net40-client,D.E.*:1.3.0\n"
-                 + "Wildcards ('*') are allowed at the end of the package id only. "
+                 + "For example, the input could be: --packages A,B.C:1.2.0:net40-client,D.E.*:1.3.0"
+                 + "Wildcards ('*') are allowed at the end of the package id only."
                  + "If version and framework is specified without --upgrade, "
                  + "they must match version/framework already installed in the solution(s), "
                  + "and all references to those packages will be aligned to that specification instead.")]
